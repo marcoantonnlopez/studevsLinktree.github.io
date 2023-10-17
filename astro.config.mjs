81ece7af9c18896a2d3974d719d8c2ebae176661
yeicob <jacobrdz34@gmail.com>
2023-10-16 21:44:14 -0600

    feat: add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+
+// https://docs.astro.build/en/guides/integrations-guide/vercel/#targets
+import vercel from "@astrojs/vercel/static"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.studevs.com",
-});
+  integrations: [tailwind()],
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    speedInsights: {
+      enabled: true,
+    },
+  }),
+})
