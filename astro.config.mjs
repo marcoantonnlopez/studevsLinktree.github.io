65d22346b5bde71edc8a06bb50da816ad70ff9d0
yeicob <jacobrdz34@gmail.com>
2023-10-16 22:17:21 -0600

    feat: add astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,22 @@
-import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 // https://docs.astro.build/en/guides/integrations-guide/vercel/#targets
-import vercel from "@astrojs/vercel/static"
+import vercel from "@astrojs/vercel/static";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.studevs.com",
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   output: "static",
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
+      enabled: true
     },
     speedInsights: {
-      enabled: true,
-    },
-  }),
-})
+      enabled: true
+    }
+  })
+});
